c57061046b875787dc12311bc9fa9ecf46e63974
Daniel C. Gomes <daniel@danielcg.net>
2019-02-17 23:13:04 -0300

    Script to delete the load balancer and target group by role

--- delete_resources.sh
+++ delete_resources.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ansible-playbook ec2_lb_delete_by_role.yml -e "ec2_region=us-east-1 ec2_role=WPServer"
+
 ansible-playbook ec2_term_by_region_role.yml -e "ec2_region=us-east-1 ec2_role=AdminJumpbox"
 
 ansible-playbook ec2_term_by_region_role.yml -e "ec2_region=us-east-1 ec2_role=WPServer"
