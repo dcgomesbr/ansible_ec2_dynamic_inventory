56624889fb25ea0938cd52770df1d7c419024a0c
Daniel C. Gomes <daniel@danielcg.net>
2019-02-28 12:29:46 -0300

    Grumble... trying to fix a problem while deleting the LB

--- delete_resources.sh
+++ delete_resources.sh
@@ -2,10 +2,10 @@
 
 ansible-playbook rds_delete_instance.yml -e "ec2_region=us-east-1 rds_role=WordPressDB"
 
-ansible-playbook ec2_lb_delete_by_role.yml -e "ec2_region=us-east-1 ec2_role=WPServer"
-
 ansible-playbook ec2_term_by_region_role.yml -e "ec2_region=us-east-1 ec2_role=AdminJumpbox"
 
 ansible-playbook ec2_term_by_region_role.yml -e "ec2_region=us-east-1 ec2_role=WPServer"
 
 ansible-playbook vpc_delete_nat_gateway.yml -e "ec2_region=us-east-1 ec2_subnet=PublicSubnetBastion"
+
+ansible-playbook ec2_lb_delete_by_role.yml -e "ec2_region=us-east-1 ec2_role=WPServer"
